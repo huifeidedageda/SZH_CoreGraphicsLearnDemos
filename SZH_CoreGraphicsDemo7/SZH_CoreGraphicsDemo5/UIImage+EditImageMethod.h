62b5c9d738aafdf00fe693539ea6706b8d27ee5b
SongZhiheng <szh13598845390@126.com>
2017-09-26 10:49:32 +0800

    实现了几种图形的绘制

---
+++ SZH_CoreGraphicsDemo7/SZH_CoreGraphicsDemo5/UIImage+EditImageMethod.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+EditImageMethod.h
+//  SZH_CoreGraphicsDemo5
+//
+//  Created by 智衡宋 on 2017/9/25.
+//  Copyright © 2017年 智衡宋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (EditImageMethod)
+
+@end
---
+++ SZH_CoreGraphicsDemo7/SZH_CoreGraphicsDemo5/UIImage+EditImageMethod.m
@@ -0,0 +1,19 @@
+//
+//  UIImage+EditImageMethod.m
+//  SZH_CoreGraphicsDemo5
+//
+//  Created by 智衡宋 on 2017/9/25.
+//  Copyright © 2017年 智衡宋. All rights reserved.
+//
+
+#import "UIImage+EditImageMethod.h"
+
+@implementation UIImage (EditImageMethod)
+
+#pragma mark ------------- 绘制图片
+
+
+
+
+
+@end
